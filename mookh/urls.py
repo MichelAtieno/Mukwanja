ce5714a4650bd41a002d2002a20cf0f761b0699c
Michel Atieno <mishqamish@gmail.com>
2019-05-28 17:27:11 +0300

    Can select single event]

--- mookh/urls.py
+++ mookh/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
-    url(r'^image/(?P<image_id>\d+)',views.single_event, name='event_gallery')
+    url(r'^image/(?P<event_id>\d+)',views.single_event, name='single_event')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
